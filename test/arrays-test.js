f60e7a0b19fe09daed855090742680f607cbcca3
Manzi <phmutman@hotmail.com>
2019-02-02 06:07:56 +0000

    Automatically backed up by Learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -87,7 +87,7 @@ describe('arrays', () => {
     it('alters the original array', ()=>{
       var array = [1, 2, 3];
       destructivelyRemoveElementFromBeginningOfArray(array);
-      expect(array.shift).to.eql([2, 3]);
+      expect(array.shift()).to.eql([2, 3]);
     })
   })
 
