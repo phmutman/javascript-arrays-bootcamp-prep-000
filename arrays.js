b9cea6aa0f33ca45b8765b795f3e5a376bfde2de
Manzi <phmutman@hotmail.com>
2019-02-02 05:17:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -31,3 +31,7 @@ function destructivelyRemoveElementFromBeginningOfArray (chocolateBars){
 function removeElementFromBeginningOfArray (chocolateBars){
   return (chocolateBars.slice(1))
 }
+
+function removeElementFromBeginningOfArray (chocolateBars){
+  return (chocolateBars.slice(1))
+}
